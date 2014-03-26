9b38e98ad4ed0c14d846c3cf84a9befd1187ff95
kranzkarol <karolkranz@gmail.com>
2014-03-26 16:20:02 +0100

    3

--- first.cpp
+++ first.cpp
@@ -4,43 +4,69 @@
 #include <dirent.h>
 using namespace std;
 
+int file__count(DIR *dp){
+	struct dirent *dirp;
+	unsigned int file_count=1;
+	while((dirp=readdir(dp))!=NULL){
+		file_count++;
+	}
+	return file_count;
+}
+int line__count(string file_name){
+	ifstream file1;
+	file1.open(file_name.c_str());
+	int line_count=1;
+	string line;
+	while(getline(file1,line)){
+		line_count++;
+	}
+	file1.close();
+	return line_count;
+}
+
 int main(int argc, char *argv[]){
 	string line;
-	//string search="407143";
-	string search=argv[1];
-	unsigned int line_count=1;
 	string file_name;
 	DIR *dp;
+	dp = opendir(".");
 	struct dirent *dirp;
-	if ((dp = opendir(".")) != NULL){
+	if (argc!=2){
+		cout<<"USAGE:\n "+string(argv[0])+" word_to_search";
+	}else{
+		string search = argv[1];
+		int file_count = file__count(dp);		
 		
-	}
-	while((dirp=readdir(dp))!=NULL){
-		if((string(dirp->d_name) != ".") and (string(dirp->d_name) != "..") and (string(dirp->d_name).find(".exe") == -1)){
-			cout<<"\nFile: "<<string(dirp->d_name)<<endl;
-			file_name=dirp->d_name;
-			ifstream file1;
-			file1.open(file_name.c_str());
-			if(file1.is_open()){
-				while(getline(file1,line)){
-					int pos = line.find(search);
-					if (pos!=-1){
-						cout<<"Line: "<<line_count;
-						cout<<", "<<line.substr(0,pos);
-						cout<<"\e[033;31m";
-						cout<<line.substr(pos,search.length());
-						cout<<"\e[033;0m";
-						cout<<line.substr(pos+search.length(),line.length())<<"\n";
-						
+		dp = opendir(".");
+		for(int j=1;j<file_count;j++){
+			unsigned char isFile=0x8;
+			dirp=readdir(dp);
+			if((string(dirp->d_name).find(".exe") == -1) and dirp->d_type == isFile){
+				cout<<"\nFile: "<<string(dirp->d_name)<<endl;
+				file_name=dirp->d_name;
+				ifstream file1;
+				int line_count = line__count(file_name);
+				file1.open(file_name.c_str());				
+				if(file1.is_open()){
+					for(int i=1;i<=line_count;i++){
+						getline(file1,line);
+						int pos = line.find(search);
+						if (pos!=-1){
+							cout<<"Line: "<<i;
+							cout<<", "<<line.substr(0,pos);
+							cout<<"\e[033;31m";
+							cout<<line.substr(pos,search.length());
+							cout<<"\e[033;0m";
+							cout<<line.substr(pos+search.length(),line.length())<<"\n";
+							
+						}
 					}
-					++line_count;
+				}else{
+					cout<<"Unable to open";
 				}
 				file1.close();
-			}else{
-				cout<<"Unable to open";
 			}
 		}
 	}
-
 	return 0;
 }
+
