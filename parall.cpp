9104b1b785bf9084dc2bd30c101e81b18bb33ba3
Karol Kranz <karolkranz@gmail.com>
2014-04-01 22:57:12 +0200

    zmiany

--- parall.cpp
+++ parall.cpp
@@ -2,32 +2,40 @@
 #include <fstream>
 #include <string>
 #include <dirent.h>
+#include <stdlib.h>
+
 using namespace std;
 
 
 int main(int argc, char *argv[]){
 	
-	string li1[]={"1","2","3"};
-	string li2[]={"4","5","6"};
-	string li3[]={"7","8","9"};
-	string fi[3][4]={*li1,*li2,*li3};
+	
 	
 	int i,j;
-	string glo;
-	#pragma omp parallel private(j) shared(fi)
-	{
-		#pragma omp for schedule(static) 
-		for(j=1;j<5;j++){
-			glo=glo+"File: "+std::to_string(j)+"\n";
-			string loc;
-			#pragma omp parallel private(i) shared(fi)
-			{
-			#pragma omp for schedule(static) 
-			for(i=1;i<5;i++){
+	int file__count=5;
+	int line__count=5;
+	string* files = new string[file__count];
+	string* lines = new string[line__count];
+	
+	int Number=1;
+	string a=itoa(Number);
+	
+	cout<<a;
+
+	#pragma omp parallel for private(a,i,j) reduction(+:files) 
+		for(i=1;i<file__count;i++){
+			// sprintf(files[i],"%s%d\n","File: ",i);
+			
+			files[i]="Files: ";
+
+			for(j=1;j<line__count;j++){
+				// sprintf(files[j],"%s%d\n","Line: ",j);
 				
 			}
-			}
 		}
-	}
+
+	cout<<files[1];
+
+
 	return 0;
 }
