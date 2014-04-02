e2f69553d2897c970ab4fed4081c7bfe07d3c037
Karol Kranz <karolkranz@gmail.com>
2014-04-02 23:59:52 +0200

    temp<< not working

--- first.cpp
+++ first.cpp
@@ -6,11 +6,14 @@
 using namespace std;
 
 int file__count(DIR *dp){
-	struct dirent *dirp;
+	struct dirent dirp;
+	struct dirent *result;
 	unsigned int file_count=1;
-	while((dirp=readdir(dp))!=NULL){
+	int return_code;
+	for (return_code = readdir_r(dp, &dirp, &result);
+         result != NULL && return_code == 0;
+         return_code = readdir_r(dp, &dirp, &result))
 		file_count++;
-	}
 	return file_count;
 }
 int line__count(string file_name){
@@ -30,7 +33,12 @@ int main(int argc, char *argv[]){
 	string file_name;
 	DIR *dp;
 	dp = opendir(".");
-	struct dirent *dirp;
+	struct dirent dirp;
+	struct dirent *result;
+
+
+
+
 	if (argc!=2){
 		cout<<"USAGE:\n "+string(argv[0])+" word_to_search";
 	}else{
@@ -44,20 +52,25 @@ int main(int argc, char *argv[]){
 		ifstream file1;
 		int line_count;
 		int pos;
-		#pragma omp parallel for private(i,j,temp,dirp,file_name,file1,line_count,line,pos,isFile)\
-		shared(output,dp, search) 
+		//#pragma omp parallel for private(i,j,temp,dirp,file_name,file1,line_count,line,pos,isFile)\
+		//shared(output,dp, search) 
 		for(i=1;i<file_count;i++){
-			dirp=readdir(dp);
-			if((string(dirp->d_name).find(".exe") == -1) and dirp->d_type == isFile){
-				temp<<"\nFile: "<<string(dirp->d_name)<<endl;
-				//cout<<"\nFile: "<<string(dirp->d_name)<<endl;
-				file_name=dirp->d_name;
+			readdir_r(dp, &dirp, &result);
+			
+			if((string(dirp.d_name).find(".exe") == -1) and dirp.d_type == isFile){
+				temp<<"\nFile: "<<i<<" "<<string(dirp.d_name)<<endl;
+				file_name=dirp.d_name;
 				line_count = line__count(file_name);
-				file1.open(file_name.c_str());				
+				file1.open(file_name.c_str());	
+				
 				if(file1.is_open()){
 					for(j=1;j<=line_count;j++){
+
 						getline(file1,line);
+
+					
 						pos = line.find(search);
+						cout<<pos;
 						if (pos!=-1){
 							temp<<"Line: "<<j;
 							temp<<", "<<line.substr(0,pos);
@@ -74,7 +87,8 @@ int main(int argc, char *argv[]){
 				file1.close();
 			}
 		}
-		cout<<output.str();
+		//cout<<output.str();
+		cout<<temp.str();
 	}
 	return 0;
 }
--- parall.cpp
+++ parall.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <stdlib.h>
 #include <dirent.h>
 #include <sstream>
-#include <dirent.h>
-#include <stdlib.h>
-
 using namespace std;
 
+int file__count(DIR *dp){
+	struct dirent dirp;
+	struct dirent *result;
+	unsigned int file_count=1;
+	int return_code;
+	for (return_code = readdir_r(dp, &dirp, &result);
+         result != NULL && return_code == 0;
+         return_code = readdir_r(dp, &dirp, &result))
+		file_count++;
+	return file_count;
+}
 
 int main(int argc, char *argv[]){
 	int x=5;
@@ -20,50 +27,38 @@ int main(int argc, char *argv[]){
 	dp = opendir(".");
 	struct dirent dirp;
 	struct dirent *result;
-	int return_code;
-	dp = opendir(".");
+
+	unsigned char isFile=0x8;
+
 	
-	for (return_code = readdir_r(dp, &dirp, &result);
-         result != NULL && return_code == 0;
-         return_code = readdir_r(dp, &dirp, &result))
-		 cout<<dirp.d_name<<endl;
-	
-	//dirp=readdir_r(dp,dirp,dirp);
-	// cout<<dirp<<endl;
-	// dirp=readdir(dp);
-	// dirp=readdir(dp);
-	// dirp=readdir(dp);
-	// dirp=readdir(dp);
-	// dirp=readdir(dp);
-	//unsigned char sd=dirp->d_type;
-	//cout<<"fefef "<<sd<<endl;
-	
-	ostringstream st;
-	
-	#pragma omp parallel for private(i,j) 
-		for(i=0;i<x;i++){
-			ostringstream loc;
-			loc<<"File: "<<i<<endl;
-			//snprintf(files[i],sizeof(files[i]),"%s %d %s","File: ",i,"\n");
-			for(j=0;j<x;j++){
-				loc<<"Line: "<<j<<endl;
-				//snprintf(lines[i],sizeof(lines[i]),"%s %d %s","Line: ",i,"\n");
-			}
-			st<<loc.str()<<endl;
+	for (i=0;i<9;i++){
+		readdir_r(dp, &dirp, &result);
+		if((string(dirp.d_name).find(".exe") == -1) and dirp.d_type == isFile){
+			
+			cout<<dirp.d_name<<" "<<dirp.d_type<<endl;
 		}
-	cout<<st.str();
-	// for(i=0;i<x;i++){
-		// cout<<files[i];
-		// for(j=0;j<x;j++){
-			// cout<<lines[j];
-		// }
-	// }
-	
-	// int num =1;
-	// string b;
-	// ostringstream st;
-	// st<<num<<' ';
-	// cout<<st.str();
+	}
+		 
+	string file_name="test";
+	string line;
+	ifstream file1;
+	file1.open(file_name.c_str());	
+	cout<<file1.is_open();
+	getline(file1,line);
+	cout<<line;
+	// #pragma omp parallel for private(i,j) 
+	// 	for(i=0;i<x;i++){
+	// 		ostringstream loc;
+	// 		loc<<"File: "<<i<<endl;
+	// 		//snprintf(files[i],sizeof(files[i]),"%s %d %s","File: ",i,"\n");
+	// 		for(j=0;j<x;j++){
+	// 			loc<<"Line: "<<j<<endl;
+	// 			//snprintf(lines[i],sizeof(lines[i]),"%s %d %s","Line: ",i,"\n");
+	// 		}
+	// 		st<<loc.str()<<endl;
+	// 	}
+	//cout<<st.str();
+
 	
 	return 0;
 }
